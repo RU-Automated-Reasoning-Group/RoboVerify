4d616d10d61442af25b99a78c5e1b86d6d0524ba
Yuning30 <1987150762@qq.com>
2025-04-24 14:26:40 -0400

    MCMC code

--- synthesis/program.py
+++ synthesis/program.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 class Parameter:
-    def __init__(self, val: int = 0):
+    def __init__(self, val: float = 0):
         self.pos = None
         self.val = val
 
@@ -17,6 +17,9 @@ class Parameter:
 
     def update(self, new_parameter: List):
         self.val = new_parameter[self.pos]
+    
+    def __str__(self):
+        return f"{self.val:.3}"
 
 
 class Instruction(ABC):
@@ -30,6 +33,16 @@ class Instruction(ABC):
     def update_trainable_parameter(self, new_parameter: List):
         pass
 
+    def get_operand(self):
+        return []
+
+    def set_operand(self, new_operands):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
 
 class Skip(Instruction):
     def __init__(self, skip_steps: int = 10):
@@ -39,34 +52,54 @@ class Skip(Instruction):
         # TODO: wee need to consider what we should do for skip
         pass
 
+    def __str__(self):
+        return "Skip"
+
 
 class PickPlace(Instruction):
-    def __init__(self, box_id: int):
-        self.box_id = box_id
-        # self.target_offset = [Parameter(0) for _ in range(3)]
-        self.target_offset = [Parameter(-0.0), Parameter(-0.2), Parameter(0.08)]
+    def __init__(self, grab_box_id: int = 0, target_box_id: int = 0, limit: int = 50):
+        self.limit = limit
+        self.grab_box_id = grab_box_id
+        self.target_box_id = target_box_id
+        self.types = ["Box", "Box"]
+        self.target_offset = [
+            Parameter(0.0) for _ in range(3)
+        ]  # target offset is applied to goal_box
+        # self.target_offset = [Parameter(-0.0), Parameter(-0.2), Parameter(0.08)]  # target offset is applied to goal_box
 
-    def eval(self, env, traj, limit: int = 50):
+    def get_box_pos(self, box_id, obs):
+        if box_id == 0:
+            return obs[10:13]
+        elif box_id == 1:
+            return obs[22:25]
+        else:
+            assert False, "unknown box id"
+
+    def eval(self, env, traj, return_img=False):
         # call the neural controller here
-        from environment.data.pickplace_dest import get_pickdest_control
+        from environment.data.pickplace_naive import get_pick_control_naive
 
+        imgs = []
         success = False
-        initial_box = traj[-1][10:13]
+        initial_goal_box = self.get_box_pos(self.target_box_id, traj[-1])
         step = 0
-        while not success and step < limit:
+        while not success and step < self.limit:
             obs = env.flatten_observation(env._get_obs())
             # import pdb; pdb.set_trace()
-            action, success = get_pickdest_control(
+            action, success = get_pick_control_naive(
                 obs,
-                initial_box + np.array([offset.val for offset in self.target_offset]),
+                initial_goal_box
+                + np.array([offset.val for offset in self.target_offset]),
                 block_id=0,
                 last_block=True,
             )
             env.step(action)
             step += 1
-            # env.render()
+            if return_img:
+                imgs.append(env.render())
             # print(step, success)
             traj.append(obs)
+        return imgs
 
     def register_trainable_parameter(self, parameter: List):
         for p in self.target_offset:
@@ -76,17 +109,38 @@ class PickPlace(Instruction):
         for p in self.target_offset:
             p.update(new_parameter)
 
+    def get_operand(self):
+        return [
+            {"type": self.types[0], "val": self.grab_box_id},
+            {"type": self.types[1], "val": self.target_box_id},
+        ]
+
+    def set_operand(self, new_operands):
+        assert new_operands[0]["type"] == "Box"
+        assert new_operands[1]["type"] == "Box"
+        self.grab_box_id = new_operands[0]["val"]
+        self.target_box_id = new_operands[1]["val"]
+
+    def __str__(self):
+        return f"PickPlace({self.grab_box_id}, {self.target_box_id}, {[str(x) for x in self.target_offset]})"
+
 
 class Program:
     def __init__(self, length: int = 5):
         self.length = length
         self.instructions = [Skip() for _ in range(self.length)]
 
-    def eval(self, env):
+    def eval(self, env, return_img=False):
         """evaluate the program in the environment and return the trajectories"""
         traj = [env.reset()[0]]
+        if return_img:
+            imgs = [env.render()]
         for line in self.instructions:
-            line.eval(env, traj)
+            line_imgs = line.eval(env, traj, return_img)
+            if return_img:
+                imgs.extend(line_imgs)
+        if return_img:
+            return traj, imgs
         return traj
 
     def register_trainable_parameter(self):
@@ -98,6 +152,10 @@ class Program:
     def update_trainable_parameter(self, new_parameter):
         for line in self.instructions:
             line.update_trainable_parameter(new_parameter)
+    
+    def __str__(self):
+        instruction_str = [f"\t{inst}" for inst in self.instructions]
+        return "\n".join(["begin", *instruction_str, "end"])
 
 
 def evaluate_program(p: Program, parameter: List):
@@ -107,7 +165,6 @@ def evaluate_program(p: Program, parameter: List):
 
 
 if __name__ == "__main__":
-
     program = Program(3)
     program.instructions = [PickPlace(0), PickPlace(0), PickPlace(0)]
     trainable_parameters = program.register_trainable_parameter()
--- synthesis/synthesis.py
+++ synthesis/synthesis.py
@@ -1,43 +1,206 @@
-import numpy as np
+import math
+import os
+import random
 
 import cem
+import ffmpeg
+import imageio
+import numpy as np
 from cost_func import *
 from environment.cee_us_env.fpp_construction_env import FetchPickAndPlaceConstruction
 from environment.data.collect_demos import CollectDemos
 from environment.general_env import GymToGymnasium
 from program import *
 
-num_seeds = 10
+
+def sample_proportional(values):
+    """
+    Given a list of positive integers, sample one index with probability
+    proportional to its value.
+
+    Parameters:
+    - values: list of positive integers
+
+    Returns:
+    - An index sampled according to the proportional distribution
+    """
+    total = sum(values)
+    probs = [v / total for v in values]
+    return random.choices(range(len(values)), weights=probs, k=1)[0]
+
+
+def swap_two_elements_maybe_same(lst):
+    """
+    Randomly selects two indices (which may be the same) and swaps their elements.
+
+    Parameters:
+    - lst: List of elements to mutate (in-place)
+
+    Returns:
+    - A new list with two elements swapped (or not, if indices are the same)
+    """
+    if len(lst) == 0:
+        raise ValueError("List must contain at least one element.")
+
+    i = random.randint(0, len(lst) - 1)
+    j = random.randint(0, len(lst) - 1)
+    lst[i], lst[j] = lst[j], lst[i]
+    return lst
+
+
+def mutate_program(current_program: Program):
+    pc = 1  # opcode
+    po = 1  # operand
+    ps = 1  # swap
+    pi = 1  # instruction
+    sampled_mutation = sample_proportional([pc, po, ps, pi])
+    print("mutation type", sampled_mutation)
+    new_program = deepcopy(current_program)
+    if sampled_mutation == 0:
+        pass
+    elif sampled_mutation == 1:
+        index = random.randint(0, len(new_program.instructions) - 1)
+        operands = new_program.instructions[index].get_operand()
+        operand_index = random.randint(0, len(operands) - 1)
+        proposed_operand = random.choice(
+            available_operands[operands[operand_index]["type"]]
+        )
+        operands[operand_index]["val"] = proposed_operand
+        new_program.instructions[index].set_operand(operands)
+    elif sampled_mutation == 2:
+        swap_two_elements_maybe_same(new_program.instructions)
+    elif sampled_mutation == 3:
+        index = random.randint(0, len(new_program.instructions) - 1)
+        pu = 0.25  # probability the SKIP token is proposed
+        if random.random() < pu:
+            new_program.instructions[index] = Skip()
+        else:
+            new_instruction = random.choice(available_instructions)()
+            operands = new_instruction.get_operand()
+            for i in range(len(operands)):
+                operands[i]["val"] = random.choice(
+                    available_operands[operands[i]["type"]]
+                )
+            new_instruction.set_operand(operands)
+            new_program.instructions[index] = new_instruction
+    else:
+        assert False, "unknown mutation"
+
+    return new_program
+
+
+def MCMC(
+    current_program: Program,
+    iters: int,
+    expert_states,
+    num_seeds: int,
+    cem_N: int = 16,
+    cem_K: int = 4,
+):
+    current_cost = optimize_program(
+        current_program, expert_states, num_seeds, cem_N, cem_K
+    )
+    current_program = current_program
+    samples = [deepcopy(current_program)]
+    costs = [current_cost]
+
+    for i in range(iters):
+        new_program = mutate_program(current_program)
+        new_cost = optimize_program(new_program)
+
+        acceptance_ratio = math.exp(current_cost - new_cost)
+        if random.random() < acceptance_ratio:
+            current_program = new_program
+            current_cost = new_cost
+
+        samples.append(deepcopy(new_program))
+        costs.append(new_cost)
+    return samples, costs
+
+
+def optimize_program(p: Program, expert_states, num_seeds: int, cem_N: int, cem_K: int):
+    f = Runner(p, expert_states, num_seeds)
+    initial_parameters = p.register_trainable_parameter()
+    best_cost = cem.cem_optimize(f, len(initial_parameters), N=cem_N, K=cem_K)
+    return best_cost
 
 
 def set_np_seed(seed: int):
     np.random.seed(seed)
 
 
-def collect_trajectories(env_name: str, n: int):
+def images_to_video(input_dir, output_video_path="output.mp4", framerate=30):
+    """
+    Convert PNG images in a directory to a video using ffmpeg.
+    Assumes images are named as img0000.png, img0001.png, ...
+
+    Parameters:
+    - input_dir: directory containing the PNG images
+    - output_video_path: output video file path
+    - framerate: frames per second for the video
+    """
+    input_pattern = os.path.join(input_dir, "img%04d.png")
+
+    try:
+        (
+            ffmpeg.input(input_pattern, framerate=framerate)
+            .output(output_video_path, vcodec="libx264", pix_fmt="yuv420p")
+            .overwrite_output()
+            .run()
+        )
+        print(f"Video saved to {output_video_path}")
+    except ffmpeg.Error as e:
+        print("FFmpeg error:", e.stderr.decode())
+
+
+def save_numpy_arrays_as_images(arrays, output_dir="images"):
+    """
+    Save a list of numpy arrays as PNG images with filenames like img0000.png, img0001.png, ...
+
+    Parameters:
+    - arrays: list of numpy arrays (each should represent an image)
+    - output_dir: directory where images will be saved
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    for idx, arr in enumerate(arrays):
+        filename = f"img{idx:04d}.png"
+        filepath = os.path.join(output_dir, filename)
+        imageio.imwrite(filepath, arr)
+
+
+def collect_trajectories(env_name: str, n: int, save_imgs=False):
     states = []
+    imgs = []
     for i in range(n):
         set_np_seed(i)
         collector = CollectDemos(
             "demo",
             traj_len=50,
             num_trajectories=1,
-            task="pickmulti",
+            task="tower",
             img_path="img",
             env_name=env_name,
-            block_num=int(1),
+            block_num=int(2),
             debug=False,
+            render=False,
         )
         obs_seq, obs_imgs = collector.collect(store=False)
+        collector.env.close()
         del collector
         for state in obs_seq[0]["obs"]:
             states.append(state)
+        for image in obs_imgs[0]:
+            imgs.append(image)
+    if save_imgs:
+        save_numpy_arrays_as_images(imgs)
     print("number of expert states", len(states))
     return states
 
 
-def evaluate_program(p: Program, n: int):
+def evaluate_program(p: Program, n: int, return_img=False):
     states = []
+    imgs = []
     for i in range(n):
         set_np_seed(i)
         env_name = "pickmulti1"
@@ -46,18 +209,24 @@ def evaluate_program(p: Program, n: int):
                 name=env_name,
                 sparse=False,
                 shaped_reward=False,
-                num_blocks=int(env_name[-1]),
+                num_blocks=int(2),
                 reward_type="sparse",
-                case="PickAndPlace",
+                case="Singletower",
                 visualize_mocap=False,
+                stack_only=True,
                 simple=True,
             ),
-            render_mode="human",
         )
-        traj = p.eval(env)
+        traj = p.eval(env, return_img)
+        if return_img:
+            traj, traj_imgs = traj
         for state in traj:
             states.append(state)
+        for image in traj_imgs:
+            imgs.append(image)
     print("number of policy states", len(states))
+    if return_img:
+        return states, imgs
     return states
 
 
@@ -101,30 +270,7 @@ class Runner:
         self.p = program
         self.expert_states = np.array(expert_states)
         self.slices = [slice(None)] * self.expert_states.ndim
-        self.slices[1] = [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            16,
-            17,
-            18,
-            19,
-            20,
-            21,
-            22,
-            23,
-            24,
-        ]
+        self.slices[1] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 22, 23, 24]
         self.slices = tuple(self.slices)
         self.expert_states = self.expert_states[self.slices]
         self.num_seeds = num_seeds
@@ -138,14 +284,53 @@ class Runner:
         return -kl_value
 
 
-expert_states = collect_trajectories("pickmulti1", num_seeds)
+if __name__ == "__main__":
+    available_operands = {
+        "Box": [0, 1, 2],
+    }
+    available_instructions = [PickPlace]
 
-p = Program(3)
-p.instructions = [PickPlace(0)]
+    # images_to_video("images", "groundtruth.mp4")
+    # exit()
 
-f = Runner(p, expert_states, num_seeds)
-initial_parameters = p.register_trainable_parameter()
-cem.cem_optimize(f, len(initial_parameters), N=16, K=5, init_mu=initial_parameters)
+    # num_seeds = 15
+
+    # expert_states = collect_trajectories("pickmulti1", num_seeds, save_imgs=True)
+    # exit()
+    p = Program(3)
+    p.instructions = [
+        PickPlace(grab_box_id=0, target_box_id=0),  # move up with respect to box 0
+        PickPlace(
+            grab_box_id=0, target_box_id=1
+        ),  # move horizontally to the top of box 1
+        PickPlace(grab_box_id=0, target_box_id=1),  # move down to place on box 1
+    ]
+    p.register_trainable_parameter()
+    p.update_trainable_parameter(
+        [
+            0.0279608,
+            0.04278932,
+            0.13399421,
+            0.02289095,
+            -0.04434892,
+            0.13288633,
+            -0.01862492,
+            0.00964001,
+            0.05832452,
+        ]
+    )
+    # p.instructions[0].target_offset = [Parameter(0.), Parameter(0.), Parameter(0.2)]
+    # p.instructions[1].target_offset = [Parameter(0), Parameter(0), Parameter(0.2)]
+    # p.instructions[2].target_offset = [Parameter(0), Parameter(0), Parameter(0.05)]
+    for _ in range(50):
+        print(mutate_program(p))
+    pdb.set_trace()
+    states, imgs = evaluate_program(p, 15, True)
+    pdb.set_trace()
+
+    f = Runner(p, expert_states, num_seeds)
+    initial_parameters = p.register_trainable_parameter()
+    cem.cem_optimize(f, len(initial_parameters), N=16, K=4, init_mu=initial_parameters)
 
 # expert_states = np.array(expert_states)[:,:3]
 # pdb.set_trace()
