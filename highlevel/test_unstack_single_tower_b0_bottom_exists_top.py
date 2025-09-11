964c80aadfd2c7c8a1d085cb73d025dd48b4ed8a
Yuning30 <1987150762@qq.com>
2025-09-10 20:32:56 -0400

    realize we can specify a top block that is also on n0 for unstack

--- highlevel/test_unstack_single_tower_b0_bottom_exists_top.py
+++ highlevel/test_unstack_single_tower_b0_bottom_exists_top.py
@@ -5,10 +5,9 @@ set_option("smt.core.minimize", "true")
 solver = Solver()
 solver.set(unsat_core=True)
 
-
-# Box = DeclareSort("Box")
+Box = DeclareSort("Box")
 # Box, (b9, b10, b11, b12, b13, b14, b15) = EnumSort('Box', ['b9', 'b10', 'b11', 'b12', 'b13', 'b14', 'b15'])
-Box, (b9, b10, b11) = EnumSort("Box", ["b9", "b10", "b11"])
+# Box, (b9, b10, b11) = EnumSort("Box", ["b9", "b10", "b11"])
 x, y, c, n0, t, next_box, x1 = Consts("x y c n0 t next_box x1", Box)
 
 # define ON_star
@@ -62,56 +61,6 @@ def on_table(x, ON_func=ON_star):
 
 def top(x, ON_func=ON_star):
     return Not(Exists([y], And(Not(y == x), ON_func(y, x))))
-
-def loop_invariant(t):
-    return And(
-        ForAll([x], Or(top(x), ON_star(x, n0))),
-        top(t),
-        ON_star(t, n0)
-    )
-    # return ForAll([x], Or(top(x), ON_star(x, n0)))
-
-def loop_invariant_substituted(next_box):
-    return ForAll([x],
-        Or(
-            top_substituted(x, next_box),
-            ON_func_substituted(x, n0, next_box)
-        )
-    )
-
-def while_cond():
-    # return (t != n0)
-    return Not(top(n0))
-
-def ON_func_substituted(alpha, beta, next_box, ON_func=ON_star):
-    return And(
-        ON_func(alpha, beta), Or(Not(ON_func(alpha, next_box)), ON_func(beta, next_box))
-    )
-
-def top_substituted(x, next_box, ON_func=ON_star):
-    return Not(
-        Exists(
-            [y], And(Not(y == x), ON_func_substituted(y, x, next_box, ON_func=ON_func))
-        )
-    )
-
-print("testing the necessity of table")
-solver.push()
-solver.assert_and_track(precondition(), "all_on_n0")
-solver.assert_and_track(Not(on_table(n0)), "not_n0_on_table")
-check_solver(solver)
-solver.pop()
-# this indicates that there's no need to have on_table(n0)
-# in precondition because it can be derived from the other part
-
-print("testing the necessity of table (2)")
-solver.push()
-solver.assert_and_track(ForAll([x], Or(top(x), ON_star(x, n0))), "top_or_on_n0")
-solver.assert_and_track(Not(on_table(n0)), "not_n0_on_table")
-check_solver(solver)
-solver.pop()
-# this indicates there's no need to have on table for n0 in the loop invariant
-# either.
 
 print("verifying precondition")
 solver.push()
